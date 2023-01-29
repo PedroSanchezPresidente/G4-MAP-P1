51c097371cdcaa7b2238adccc99742cf88f34169
marcrest <123576559+marcrest@users.noreply.github.com>
2023-01-29 19:11:53 +0100

    Marcos Pantoja
    crear el goomba con detección de colisiones(mata al goomba cuando salta encima y rebota. Colisiona con el tubo y cambia su sentido)

--- Assets/Scripts/MovementComponent.cs
+++ Assets/Scripts/MovementComponent.cs
@@ -71,10 +71,7 @@ public class MovementComponent : MonoBehaviour
             _jump = false;
 
         }
-        else if (_fall)
-        {
-            _rigidbody2D.AddForce(Vector2.down * _downForce);
-        }
+        
     }
     
 
