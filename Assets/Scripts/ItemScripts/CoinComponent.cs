76ed4dcdac00cd8fa93cf043557df3f1bc480229
Inés <ine.primo.lpz@gmail.com>
2023-02-08 22:53:13 +0100

    añadido destroy de la moneda despues de la animacion

--- Assets/Scripts/ItemScripts/CoinComponent.cs
+++ Assets/Scripts/ItemScripts/CoinComponent.cs
@@ -8,6 +8,8 @@ public class CoinComponent : MonoBehaviour
     public void GetCoin()
     {
         GameManager.Instance.OnPickCoin();
+        //destruye el objeto despues de la animacion (+ un delay ???)
+        Destroy(gameObject, GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length + 1);  
     }
 
     // Start is called before the first frame update
